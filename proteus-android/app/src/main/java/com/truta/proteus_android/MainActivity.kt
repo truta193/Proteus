49a8e943d3354caa4637b2cd7842c767997d18df
truta193 <trutaandrei7033@gmail.com>
2024-02-22 23:26:02 +0200

    Firebase auth working

--- proteus-android/app/src/main/java/com/truta/proteus_android/MainActivity.kt
+++ proteus-android/app/src/main/java/com/truta/proteus_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.truta.proteus_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.truta.proteus_android.ui.RegistrationScreen
@@ -15,8 +16,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val db = Firebase.firestore
-        db.useEmulator("192.168.0.122", 8080)
+        Firebase.firestore.useEmulator("192.168.0.122", 8080)
+        //Firebase.auth.useEmulator("192.168.0.122", 9099)
         
         setContent {
             ProteusAndroidTheme {
--- proteus-android/app/src/main/java/com/truta/proteus_android/MainApplication.kt
+++ proteus-android/app/src/main/java/com/truta/proteus_android/MainApplication.kt
@@ -1,8 +1,14 @@
 package com.truta.proteus_android
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
 }
